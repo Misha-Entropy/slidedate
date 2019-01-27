ada877bb9f1e3e62d6d57d182b660253633c9b55
macbook <wimanart@gmail.com>
2019-01-27 21:55:19 +0300

    1

--- app/src/main/java/phontm/expertdate/ui/SplashActivity.java
+++ app/src/main/java/phontm/expertdate/ui/SplashActivity.java
@@ -1,7 +1,11 @@
 package phontm.expertdate.ui;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Handler;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -95,8 +99,39 @@ public class SplashActivity extends AppCompatActivity {
         super.onPause();
     }
 
+
+   public  boolean isStoragePermissionGranted() {
+       if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+           if (checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                   == PackageManager.PERMISSION_GRANTED) {
+               Log.v("LAUNCH", "Permission is granted");
+               return true;
+           } else {
+
+               Log.v("LAUNCH", "Permission is revoked");
+               ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+               return false;
+           }
+       } else { //permission is automatically granted on sdk<23 upon installation
+           Log.v("LAUNCH", "Permission is granted");
+           return true;
+       }
+   }
+
     private synchronized void checkReadyToLaunch() {
         if (mTimeIsOut) {
+            if (isStoragePermissionGranted()) {
+                goToMainActivity();
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(grantResults[0]== PackageManager.PERMISSION_GRANTED){
+            Log.v("LAUNCH","Permission: "+permissions[0]+ "was "+grantResults[0]);
+            //resume tasks needing this permission
             goToMainActivity();
         }
     }
--- app/src/main/java/phontm/expertdate/ui/WebActivity.java
+++ app/src/main/java/phontm/expertdate/ui/WebActivity.java
@@ -1,5 +1,11 @@
 package phontm.expertdate.ui;
 
+import android.annotation.TargetApi;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,12 +13,65 @@ import android.view.Window;
 import android.view.WindowManager;
 import android.webkit.CookieManager;
 import android.webkit.CookieSyncManager;
+import android.webkit.ValueCallback;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import phontm.expertdate.R;
 
 public class WebActivity extends AppCompatActivity {
+
+    WebView asw_view;
+    ProgressBar asw_progress;
+    TextView asw_loading_text;
+    NotificationManager asw_notification;
+    Notification asw_notification_new;
+
+    private String asw_cam_message;
+    private ValueCallback<Uri> asw_file_message;
+    private ValueCallback<Uri[]> asw_file_path;
+    private final static int asw_file_req = 1;
+
+    private final static int loc_perm = 1;
+    private final static int file_perm = 2;
+
+
+    private ValueCallback<Uri> mUploadMessage;
+    public ValueCallback<Uri[]> uploadMessage;
+    public static final int REQUEST_SELECT_FILE = 100;
+    private final static int FILECHOOSER_RESULTCODE = 1;
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent intent)
+    {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
+        {
+            if (requestCode == REQUEST_SELECT_FILE)
+            {
+                if (uploadMessage == null)
+                    return;
+                uploadMessage.onReceiveValue(WebChromeClient.FileChooserParams.parseResult(resultCode, intent));
+                uploadMessage = null;
+            }
+        }
+        else if (requestCode == FILECHOOSER_RESULTCODE)
+        {
+            if (null == mUploadMessage)
+                return;
+            // Use MainActivity.RESULT_OK if you're implementing WebView inside Fragment
+            // Use RESULT_OK only if you're implementing WebView inside an Activity
+            Uri result = intent == null || resultCode != RESULT_OK ? null : intent.getData();
+            mUploadMessage.onReceiveValue(result);
+            mUploadMessage = null;
+        }
+        else
+            Toast.makeText(getApplicationContext(), "Failed to Upload Image", Toast.LENGTH_LONG).show();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,23 +82,88 @@ public class WebActivity extends AppCompatActivity {
         setContentView(R.layout.activity_web);
         CookieSyncManager.createInstance(this);
         CookieSyncManager.getInstance().startSync();
-        WebView webview = findViewById(R.id.webview);
-        webview.getSettings().setJavaScriptEnabled(true);
-        webview.getSettings().setAllowContentAccess(true);
-        webview.getSettings().setAppCacheEnabled(true);
+        asw_view = findViewById(R.id.webview);
+        asw_view.getSettings().setJavaScriptEnabled(true);
+        asw_view.getSettings().setAllowContentAccess(true);
+        asw_view.getSettings().setAppCacheEnabled(true);
         CookieManager cookieManager = CookieManager.getInstance();
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            cookieManager.setAcceptThirdPartyCookies(webview,true);
+            cookieManager.setAcceptThirdPartyCookies(asw_view,true);
         } else {
             cookieManager.setAcceptCookie(true);
         }
-        webview.setWebViewClient(new WebViewClient() {
+        WebSettings mWebSettings = asw_view.getSettings();
+        mWebSettings.setJavaScriptEnabled(true);
+        mWebSettings.setSupportZoom(false);
+        mWebSettings.setAllowFileAccess(true);
+        mWebSettings.setAllowFileAccess(true);
+        mWebSettings.setAllowContentAccess(true);
+
+        asw_view.setWebChromeClient(new WebChromeClient()
+        {
+            // For 3.0+ Devices (Start)
+            // onActivityResult attached before constructor
+            protected void openFileChooser(ValueCallback uploadMsg, String acceptType)
+            {
+                mUploadMessage = uploadMsg;
+                Intent i = new Intent(Intent.ACTION_GET_CONTENT);
+                i.addCategory(Intent.CATEGORY_OPENABLE);
+                i.setType("image/*");
+                startActivityForResult(Intent.createChooser(i, "File Browser"), FILECHOOSER_RESULTCODE);
+            }
+
+
+            // For Lollipop 5.0+ Devices
+            @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+            public boolean onShowFileChooser(WebView mWebView, ValueCallback<Uri[]> filePathCallback, WebChromeClient.FileChooserParams fileChooserParams)
+            {
+                if (uploadMessage != null) {
+                    uploadMessage.onReceiveValue(null);
+                    uploadMessage = null;
+                }
+
+                uploadMessage = filePathCallback;
+
+                Intent intent = fileChooserParams.createIntent();
+                try
+                {
+                    startActivityForResult(intent, REQUEST_SELECT_FILE);
+                } catch (ActivityNotFoundException e)
+                {
+                    uploadMessage = null;
+                    Toast.makeText(getApplicationContext(), "Cannot Open File Chooser", Toast.LENGTH_LONG).show();
+                    return false;
+                }
+                return true;
+            }
+
+            //For Android 4.1 only
+            protected void openFileChooser(ValueCallback<Uri> uploadMsg, String acceptType, String capture)
+            {
+                mUploadMessage = uploadMsg;
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.addCategory(Intent.CATEGORY_OPENABLE);
+                intent.setType("image/*");
+                startActivityForResult(Intent.createChooser(intent, "File Browser"), FILECHOOSER_RESULTCODE);
+            }
+
+            protected void openFileChooser(ValueCallback<Uri> uploadMsg)
+            {
+                mUploadMessage = uploadMsg;
+                Intent i = new Intent(Intent.ACTION_GET_CONTENT);
+                i.addCategory(Intent.CATEGORY_OPENABLE);
+                i.setType("image/*");
+                startActivityForResult(Intent.createChooser(i, "File Chooser"), FILECHOOSER_RESULTCODE);
+            }
+        });
+
+        asw_view.setWebViewClient(new WebViewClient() {
             @Override
             public boolean shouldOverrideUrlLoading(WebView view, String url)
             {
                 return false;
             }
         });
-        webview.loadUrl(getIntent().getStringExtra("url"));
+        asw_view.loadUrl(getIntent().getStringExtra("url"));
     }
 }
